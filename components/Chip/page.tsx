e9209321eac61cc3d81717d8e17e0654dfc21d1f
Denis Leme <denis_leme@icloud.com>
2023-10-26 15:48:01 -0300

    feat:Radio Button and refactor

--- components/Chip/page.tsx
+++ components/Chip/page.tsx
@@ -1,47 +1,77 @@
-// import Chip from "@material-tailwind/react/components/Chip";
-// import { useState } from "react";
+import React from "react";
 
-// type ChipValue = "OPC 1" | "OPC 2" | "OPC 3" | "OPC 4" | "OPC 5";
+type ChipValue = "OPC 1" | "OPC 2" | "OPC 3" | "OPC 4" | "OPC 5" | 'OPC 6' | "OPC 7" | "OPC 8";
 
-// export function ChipVariants() {
-//   const [selectedChips, setSelectedChips] = useState<ChipValue[]>([]);
+interface CustomChipProps {
+  value: ChipValue;
+  onClick: () => void;
+  selected: boolean;
+}
 
-//   const handleChipClick = (chipValue: ChipValue) => {
-//     if (selectedChips.includes(chipValue)) {
-//       setSelectedChips(selectedChips.filter((value) => value !== chipValue));
-//     } else {
-//       setSelectedChips([...selectedChips, chipValue]);
-//     }
-//   };
+const CustomChip: React.FC<CustomChipProps> = ({ value, onClick, selected }) => {
+  return (
+    <div className={`rounded-full p-2 cursor-pointer ${
+        selected ? "border border-slate-900" : "border border-blue-500"
+      }`} onClick={onClick}>
+      {value}
+    </div>
+  );
+};
 
-//   return (
-//     <div className="flex gap-4">
-//       <Chip
-//         variant={selectedChips.includes("OPC 1") ? "filled" : "outlined"}
-//         value="OPC 1"
-//         onClick={() => handleChipClick("OPC 1")}
-//       />
-//       <Chip
-//         variant={selectedChips.includes("OPC 2") ? "filled" : "outlined"}
-//         value="OPC 2"
-//         onClick={() => handleChipClick("OPC 2")}
-//       />
-//         <Chip
-//         variant={selectedChips.includes("OPC 3") ? "filled" : "outlined"}
-//         value="OPC 3"
-//         onClick={() => handleChipClick("OPC 3")}
-//       />
-//       <Chip
-//         variant={selectedChips.includes("OPC 4") ? "filled" : "outlined"}
-//         value="OPC 4"
-//         onClick={() => handleChipClick("OPC 4")}
-//       />
-//       <Chip
-//         variant={selectedChips.includes("OPC 5") ? "filled" : "outlined"}
-//         value="OPC 5"
-//         onClick={() => handleChipClick("OPC 5")}
-//       />
-     
-//     </div>
-//   );
-// }
+export function ChipVariants() {
+  const [selectedChips, setSelectedChips] = React.useState<ChipValue[]>([]);
+
+  const handleChipClick = (chipValue: ChipValue) => {
+    if (selectedChips.includes(chipValue)) {
+      setSelectedChips(selectedChips.filter((value) => value !== chipValue));
+    } else {
+      setSelectedChips([...selectedChips, chipValue]);
+    }
+  };
+
+  return (
+    <div className="flex gap-4 h-8 rounded-md">
+        <br/>
+      <CustomChip
+        value="OPC 1"
+        selected={selectedChips.includes("OPC 1")}
+        onClick={() => handleChipClick("OPC 1")}
+      />
+      <CustomChip
+        value="OPC 2"
+        selected={selectedChips.includes("OPC 2")}
+        onClick={() => handleChipClick("OPC 2")}
+      />
+      <CustomChip
+        value="OPC 3"
+        selected={selectedChips.includes("OPC 3")}
+        onClick={() => handleChipClick("OPC 3")}
+      />
+      <CustomChip
+        value="OPC 4"
+        selected={selectedChips.includes("OPC 4")}
+        onClick={() => handleChipClick("OPC 4")}
+      />
+      <CustomChip
+        value="OPC 5"
+        selected={selectedChips.includes("OPC 5")}
+        onClick={() => handleChipClick("OPC 5")}
+      />
+      <CustomChip
+        value="OPC 6"
+        selected={selectedChips.includes("OPC 6")}
+        onClick={() => handleChipClick("OPC 6")}
+      />
+      <CustomChip
+        value="OPC 7"
+        selected={selectedChips.includes("OPC 7")}
+        onClick={() => handleChipClick("OPC 7")}
+      />
+      <CustomChip
+        value="OPC 8"
+        selected={selectedChips.includes("OPC 8")}
+        onClick={() => handleChipClick("OPC 8")}
+      />
+    </div>
+  );
+}
--- components/Form/page.tsx
+++ components/Form/page.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { use, useState } from 'react';
 import StarRatings from 'react-star-ratings';
 import Checkbox from "../Checkbox/page";
 import InputComponent from '../Input/page';
+import { ChipVariants } from '../Chip/page';
+import RadioButton from '../RadioButton/page';
+import Dropdown from '../Dropdown/page';
 
 // import { ChipVariants } from '../Chip/page';
 // import Dropdown from '../Dropdown/page';
@@ -10,29 +13,52 @@ import InputComponent from '../Input/page';
 const SatisfacaoForm = () => {
   const [rating, setRating] = useState(0);
 
+  const[radioValue , setRadioValue] = useState({
+    radio1: false,
+    radio2: false,
+    radio3: false,
+    radio4: false,
+    radio5: false,
+    radio6: false,
+    radio7: false,
+    radio8: false,
+    radio9: false,
+    radio10: false,
+  })
+
   const [checkboxValue, setCheckboxValue] = useState({
     checkbox1: false,
-    checkbox2:false,
-    checkbox3:false,
-    checkbox4:false,
-    checkbox5:false,
-    checkbox6:false,
-    checkbox7:false,
-    checkbox8:false,
-    checkbox9:false,
-    checkbox10:false,
-    checkbox11:false,
-    checkbox12:false,
-    checkbox13:false,
-    checkbox14:false,
-    checkbox15:false,
-    checkbox16:false,
-    checkbox17:false,
-    checkbox18:false,
+    checkbox2: false,
+    checkbox3: false,
+    checkbox4: false,
+    checkbox5: false,
+    checkbox6: false,
+    checkbox7: false,
+    checkbox8: false,
+    checkbox9: false,
+    checkbox10: false,
+    checkbox11: false,
+    checkbox12: false,
+    checkbox13: false,
+    checkbox14: false,
+    checkbox15: false,
+    checkbox16: false,
+    checkbox17: false,
+    checkbox18: false,
+    checkbox19: false,
+    checkbox20: false,
+    checkbox21: false,
+    checkbox22: false,
   });
   const handleCheckboxChange = (name: string, isChecked: boolean) => {
     setCheckboxValue({
       ...checkboxValue,
+      [name]: isChecked,
+    });
+  };
+  const handleRadioChange = (name: string, isChecked: boolean) => {
+    setRadioValue({
+      ...radioValue,
       [name]: isChecked,
     });
   };
@@ -83,6 +109,7 @@ const SatisfacaoForm = () => {
                 </div>
               </div>
 
+
               <div id="second-question">
                 <h1 className="text-[24px] my-2">
                   Título da pergunta deve ficar aqui
@@ -92,223 +119,255 @@ const SatisfacaoForm = () => {
                   <br />da pergunta para ajudar o entendimento do usuário
                 </p>
                 <div className="flex gap-2 py-4 rounded-full justify-between">
-                  <Checkbox
+                  <RadioButton
                     label="1"
                     name="1"
                     isFlexRow={false}
                     isRounded={true}
-                    isChecked={checkboxValue.checkbox1}
+                    isChecked={radioValue.radio1}
                     onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox1", isChecked)
-                    }
-                  />
-                   <Checkbox
+                      handleRadioChange("radio1", isChecked)
+                    } />
+                  <RadioButton
                     label="2"
                     name="2"
                     isFlexRow={false}
                     isRounded={true}
-                    isChecked={checkboxValue.checkbox2}
+                    isChecked={radioValue.radio2}
                     onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox2", isChecked)
-                    }
-                  />
-                   <Checkbox
+                      handleRadioChange("radio2", isChecked)
+                    } />
+                  <RadioButton
                     label="3"
                     name="3"
                     isFlexRow={false}
                     isRounded={true}
-                    isChecked={checkboxValue.checkbox3}
+                    isChecked={radioValue.radio3}
                     onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox3", isChecked)
-                    }
-                  />
-                  <Checkbox
+                      handleRadioChange("radio3", isChecked)
+                    } />
+                  <RadioButton
                     label="4"
                     name="4"
                     isFlexRow={false}
                     isRounded={true}
-                    isChecked={checkboxValue.checkbox4}
+                    isChecked={radioValue.radio4}
                     onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox4", isChecked)
-                    }
-                  />
-                  <Checkbox
+                      handleRadioChange("radio4", isChecked)
+                    } />
+                  <RadioButton
                     label="5"
                     name="5"
                     isFlexRow={false}
                     isRounded={true}
-                    isChecked={checkboxValue.checkbox5}
+                    isChecked={radioValue.radio5}
                     onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox5", isChecked)
-                    }
-                  />
-                  <Checkbox
+                      handleRadioChange("radio5", isChecked)
+                    } />
+                  <RadioButton
                     label="6"
                     name="6"
                     isFlexRow={false}
                     isRounded={true}
-                    isChecked={checkboxValue.checkbox6}
+                    isChecked={radioValue.radio6}
                     onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox6", isChecked)
-                    }
-                  />
-                  <Checkbox
+                      handleRadioChange("radio6", isChecked)
+                    } />
+                  <RadioButton
                     label="7"
                     name="7"
                     isFlexRow={false}
                     isRounded={true}
-                    isChecked={checkboxValue.checkbox7}
+                    isChecked={radioValue.radio7}
                     onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox7", isChecked)
-                    }
-                  />
-                  <Checkbox
+                      handleRadioChange("radio7", isChecked)
+                    } />
+                  <RadioButton
                     label="8"
                     name="8"
                     isFlexRow={false}
                     isRounded={true}
-                    isChecked={checkboxValue.checkbox8}
+                    isChecked={radioValue.radio8}
                     onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox8", isChecked)
-                    }
-                  />
-                  <Checkbox
+                      handleRadioChange("radio8", isChecked)
+                    } />
+                  <RadioButton
                     label="9"
                     name="9"
                     isFlexRow={false}
                     isRounded={true}
-                    isChecked={checkboxValue.checkbox9}
+                    isChecked={radioValue.radio9}
                     onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox9", isChecked)
-                    }
-                  />
-                  <Checkbox
+                      handleRadioChange("radio9", isChecked)
+                    } />
+                  <RadioButton
                     label="10"
                     name="10"
                     isFlexRow={false}
                     isRounded={true}
-                    isChecked={checkboxValue.checkbox9}
+                    isChecked={radioValue.radio10}
                     onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox10", isChecked)
-                    }
-                  />
+                      handleRadioChange("radio10", isChecked)
+                    } />
                 </div>
                 <div>
-                <p className="text-[14px] text-muted my-2">
-                  Descreva o motivo da sua avaliação
-                  <span className='text-gray-500'>&nbsp;(opcional)</span>
-                </p>
-                <InputComponent/>
+                  <p className="text-[14px] text-muted my-2">
+                    Descreva o motivo da sua avaliação
+                    <span className='text-gray-500'>&nbsp;(opcional)</span>
+                  </p>
+                  <InputComponent />
                 </div>
-                 {/* <div>
+                <div>
                   <Dropdown/>
-                </div>  */}
+                </div> 
                 <div className='text-[14px] text-muted my-2 '>
                   <p>Pergunta de escolha única?</p>
-                  <div className='flex gap-10 py-4 rounded-full justify-start'> 
-                  <Checkbox
-                    label="Sim"
-                    name="Sim"
-                    isFlexRow={false}
-                    isRounded={true}
-                    isChecked={checkboxValue.checkbox11}
-                    onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox11", isChecked)
-                    }
-                  />
-                  <Checkbox
-                    label="Não"
-                    name="Não"
-                    isFlexRow={false}
-                    isRounded={true}
-                    isChecked={checkboxValue.checkbox12}
-                    onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox12", isChecked)
-                    }
-                  />
-                  </div>       
+                  <div className='flex gap-10 py-4 rounded-full justify-start'>
+                    <Checkbox
+                      label="Sim"
+                      name="Sim"
+                      isFlexRow={false}
+                      isRounded={true}
+                      isChecked={checkboxValue.checkbox11}
+                      onChange={(isChecked: any) =>
+                        handleCheckboxChange("checkbox11", isChecked)
+                      }
+                    />
+                    <Checkbox
+                      label="Não"
+                      name="Não"
+                      isFlexRow={false}
+                      isRounded={true}
+                      isChecked={checkboxValue.checkbox12}
+                      onChange={(isChecked: any) =>
+                        handleCheckboxChange("checkbox12", isChecked)
+                      }
+                    />
+                  </div>
                 </div>
-                {/* <div className='text-[14px] text-muted my-2'>
+                <div className='text-[14px] text-muted my-2'>
                   <p>Pergunta de multipla escolha?</p>
                   <div>
-                    <ChipVariants/>
-                  </div>         
-                </div> */}
+                    <br />
+                    <ChipVariants />
+                  </div>
+                </div>
+                <br />
                 <div className='text-[14px] text-muted my-2  '>
                   <p>Pergunta de multipla escolha?</p>
-                  <div className='flex flex-col gap-2 py-2 rounded-full items-start'> 
-                  <Checkbox
-                    label="OPC1"
-                    name="OPC1"
-                    isFlexRow={false}
-                    isRounded={true}
-                    isChecked={checkboxValue.checkbox13}
-                    onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox13", isChecked)
-                    }
-                  />
-                  <Checkbox
-                    label="OPC2"
-                    name="OPC2"
-                    isFlexRow={false}
-                    isRounded={true}
-                    isChecked={checkboxValue.checkbox14}
-                    onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox14", isChecked)
-                    }
-                  />
-                  <Checkbox
-                    label="OPC3"
-                    name="OPC3"
-                    isFlexRow={false}
-                    isRounded={true}
-                    isChecked={checkboxValue.checkbox15}
-                    onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox15", isChecked)
-                    }
-                  />
-                   <Checkbox
-                    label="OPC4"
-                    name="OPC4"
-                    isFlexRow={false}
-                    isRounded={true}
-                    isChecked={checkboxValue.checkbox16}
-                    onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox16", isChecked)
-                    }
-                  />
-                   <Checkbox
-                    label="OPC5"
-                    name="OPC5"
-                    isFlexRow={false}
-                    isRounded={true}
-                    isChecked={checkboxValue.checkbox17}
-                    onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox17", isChecked)
-                    }
-                  />
-                  <Checkbox
-                    label="OPC6"
-                    name="OPC6"
-                    isFlexRow={false}
-                    isRounded={true}
-                    isChecked={checkboxValue.checkbox18}
-                    onChange={(isChecked: any) =>
-                      handleCheckboxChange("checkbox18", isChecked)
-                    }
-                  />
-                  </div>       
+                  <div className='flex flex-col gap-2 py-2 rounded-full items-start'>
+                    <Checkbox
+                      label="OPC1"
+                      name="OPC1"
+                      isFlexRow={false}
+                      isRounded={true}
+                      isChecked={checkboxValue.checkbox13}
+                      onChange={(isChecked: any) =>
+                        handleCheckboxChange("checkbox13", isChecked)
+                      }
+                    />
+                    <Checkbox
+                      label="OPC2"
+                      name="OPC2"
+                      isFlexRow={false}
+                      isRounded={true}
+                      isChecked={checkboxValue.checkbox14}
+                      onChange={(isChecked: any) =>
+                        handleCheckboxChange("checkbox14", isChecked)
+                      }
+                    />
+                    <Checkbox
+                      label="OPC3"
+                      name="OPC3"
+                      isFlexRow={false}
+                      isRounded={true}
+                      isChecked={checkboxValue.checkbox15}
+                      onChange={(isChecked: any) =>
+                        handleCheckboxChange("checkbox15", isChecked)
+                      }
+                    />
+                    <Checkbox
+                      label="OPC4"
+                      name="OPC4"
+                      isFlexRow={false}
+                      isRounded={true}
+                      isChecked={checkboxValue.checkbox16}
+                      onChange={(isChecked: any) =>
+                        handleCheckboxChange("checkbox16", isChecked)
+                      }
+                    />
+                    <Checkbox
+                      label="OPC5"
+                      name="OPC5"
+                      isFlexRow={false}
+                      isRounded={true}
+                      isChecked={checkboxValue.checkbox17}
+                      onChange={(isChecked: any) =>
+                        handleCheckboxChange("checkbox17", isChecked)
+                      }
+                    />
+                    <Checkbox
+                      label="OPC6"
+                      name="OPC6"
+                      isFlexRow={false}
+                      isRounded={true}
+                      isChecked={checkboxValue.checkbox18}
+                      onChange={(isChecked: any) =>
+                        handleCheckboxChange("checkbox18", isChecked)
+                      }
+                    />
+                    <Checkbox
+                      label="OPC7"
+                      name="OPC7"
+                      isFlexRow={false}
+                      isRounded={true}
+                      isChecked={checkboxValue.checkbox19}
+                      onChange={(isChecked: any) =>
+                        handleCheckboxChange("checkbox19", isChecked)
+                      }
+                    />
+                    <Checkbox
+                      label="OPC8"
+                      name="OPC8"
+                      isFlexRow={false}
+                      isRounded={true}
+                      isChecked={checkboxValue.checkbox20}
+                      onChange={(isChecked: any) =>
+                        handleCheckboxChange("checkbox20", isChecked)
+                      }
+                    />
+                    <Checkbox
+                      label="OPC9"
+                      name="OPC9"
+                      isFlexRow={false}
+                      isRounded={true}
+                      isChecked={checkboxValue.checkbox21}
+                      onChange={(isChecked: any) =>
+                        handleCheckboxChange("checkbox21", isChecked)
+                      }
+                    />
+                    <Checkbox
+                      label="OPC10"
+                      name="OPC10"
+                      isFlexRow={false}
+                      isRounded={true}
+                      isChecked={checkboxValue.checkbox22}
+                      onChange={(isChecked: any) =>
+                        handleCheckboxChange("checkbox22", isChecked)
+                      }
+                    />
+                  </div>
                 </div>
                 <div>
-                <p className="text-[14px] text-muted my-2">
-                  Pergunta de texto?
-                </p>
-                <InputComponent/>
+                  <p className="text-[14px] text-muted my-2">
+                    Pergunta de texto?
+                  </p>
+                  <InputComponent />
                 </div>
-                <br/>
+                <br />
                 <div>
                   <p className='text-[14px] text-muted my-2'>Pergunta de texto?</p>
-                  <InputComponent/>
+                  <InputComponent />
                 </div>
               </div>
 
