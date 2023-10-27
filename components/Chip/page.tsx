9cdac10863253180351fefe646a24e1b447c7af0
Denis Leme <denis_leme@icloud.com>
2023-10-27 10:06:14 -0300

    fix:Chip shadow

--- components/Chip/page.tsx
+++ components/Chip/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 type ChipValue = "OPC 1" | "OPC 2" | "OPC 3" | "OPC 4" | "OPC 5" | 'OPC 6' | "OPC 7" | "OPC 8";
 
@@ -9,10 +9,15 @@ interface CustomChipProps {
 }
 
 const CustomChip: React.FC<CustomChipProps> = ({ value, onClick, selected }) => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <div className={`rounded-full p-2 cursor-pointer ${
-        selected ? "border border-blue-500" : "border border-slate-900" 
-      }`} onClick={onClick}>
+    <div className={`rounded-full p-2 cursor-pointer border transition duration-300 ${
+      selected ? "border-blue-500 shadow-lg bg-blue-100" : 
+      isHovered ? "border-blue-300 shadow-md bg-blue-50" : 
+      "border-slate-900"
+    }`} onClick={onClick}
+    onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}>
       {value}
     </div>
   );
@@ -31,47 +36,14 @@ export function ChipVariants() {
 
   return (
     <div className="flex gap-4 h-8 rounded-md">
-        
-      <CustomChip
-        value="OPC 1"
-        selected={selectedChips.includes("OPC 1")}
-        onClick={() => handleChipClick("OPC 1")}
-      />
-      <CustomChip
-        value="OPC 2"
-        selected={selectedChips.includes("OPC 2")}
-        onClick={() => handleChipClick("OPC 2")}
-      />
-      <CustomChip
-        value="OPC 3"
-        selected={selectedChips.includes("OPC 3")}
-        onClick={() => handleChipClick("OPC 3")}
-      />
-      <CustomChip
-        value="OPC 4"
-        selected={selectedChips.includes("OPC 4")}
-        onClick={() => handleChipClick("OPC 4")}
-      />
-      <CustomChip
-        value="OPC 5"
-        selected={selectedChips.includes("OPC 5")}
-        onClick={() => handleChipClick("OPC 5")}
-      />
-      <CustomChip
-        value="OPC 6"
-        selected={selectedChips.includes("OPC 6")}
-        onClick={() => handleChipClick("OPC 6")}
-      />
-      <CustomChip
-        value="OPC 7"
-        selected={selectedChips.includes("OPC 7")}
-        onClick={() => handleChipClick("OPC 7")}
-      />
-      <CustomChip
-        value="OPC 8"
-        selected={selectedChips.includes("OPC 8")}
-        onClick={() => handleChipClick("OPC 8")}
-      />
+      {["OPC 1", "OPC 2", "OPC 3", "OPC 4", "OPC 5", "OPC 6", "OPC 7", "OPC 8"].map((value) => (
+        <CustomChip
+          key={value}
+          value={value as ChipValue}
+          selected={selectedChips.includes(value as ChipValue)}
+          onClick={() => handleChipClick(value as ChipValue)}
+        />
+      ))}
     </div>
   );
 }
